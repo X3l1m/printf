b872d8bde76e10df655bd640d937f2fdb549302c
Selim Yildirim <seyildir@f1r4s9.codam.nl>
2023-01-13 17:39:41 +0100

    ev

--- dene.c
+++ dene.c
@@ -1,7 +1,21 @@
 #include <unistd.h>
+#include <stdarg.h>
+#include "libft/libft.h"
+
+void	func(char *set, ...)
+{
+	va_list arg;
+	va_start(arg, set);
+	printf("%s", set);
+	printf("%s\n", va_arg(arg, char*));
+	int it = va_arg(arg, int);
+	write(1, &it, 1);
+	va_end(arg);
+}
+
 
 int main()
 {
-	char a[] = "domates";
-	write(1, *a, 3);
+	//char hello[] = "hella";
+	func("hello", "fdks", 's');
 }
--- ft_printf.c
+++ ft_printf.c
@@ -4,25 +4,44 @@
 #include "libft/libft.h"
 
 
+int	chartyp(va_list arg)
+{
+	char arg_c;
+	arg_c = va_arg(arg, int);
+	write(1, &arg_c, 1);
+	return(1);
+}
+
+int	percent(char typef, va_list arg)
+{
+	if(typef == 'c')
+		chartyp(arg);
+	return (1);
+}
+
 int ft_printf(const char *typef, ...)
 {
+	va_list arg;
+	va_start(arg, typef);
+	int	len = 0;
 	while (*typef)
 	{
 		if (*typef == '%')
 		{
-			write(1, "bok", 3);
+			if(!*++typef)
+				break;
+			percent(*typef++, arg);
+			write(1, "&", 1);
 		}
-		
 		write(1, typef++, 1);
+		len++;
 	}
-
-
-	int lenprint = 1;
-	return (lenprint);
+	va_end(arg);
+	return (len);
 }
 
 int main()
 {
-	ft_printf("h%ello", 2, 5, 5, 6, 4);
-	//printf("%C", 'k');
+	ft_printf("h%cllo%\0a", 48, 5, 5, 6, 4);
+	printf("h%cllo%\0a", 48);
 }
